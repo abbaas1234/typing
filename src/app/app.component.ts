da4d88d3aa714ed9afc20aeea05dce65ab446a6f
abbaas1234 <johndoe@example.com>
2024-01-18 15:14:40 +0330

    Typing-init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import sentence from '@fakerjs/sentence';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,22 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'typing';
+  randomText : string = sentence({words: 10});
+  inputText : string = '';
+
+  onTextChanged(text: string) {
+    this.inputText = text;
+  }
+
+  getClass(randomLetter: string, inputLetter: string) {
+    if (!inputLetter) {
+      return 'pending'
+    }
+
+    if (randomLetter === inputLetter) {
+      return 'correct'
+    } else {
+      return 'incorrect'
+    }
+  }
 }
